0b5db6354203e58e7bdfc84c1a7d17ca3d9b45b7
zhaolu <zhaolu@xiaojubianli.com>
2019-08-26 22:31:45 +0800

    [master] 完成用户添加查询

--- src/views/account/role.vue
+++ src/views/account/role.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="app-container">
-    <el-button
-      type="primary"
-      @click="handleAddRole"
-    >新建角色</el-button>
+    <el-button type="primary" @click="handleAddRole">新建角色</el-button>
 
     <el-table
       v-loading="listLoading"
@@ -14,59 +11,27 @@
       style="width: 100%;margin-top:30px;"
       border
     >
-      <el-table-column
-        align="center"
-        label="编号"
-        width="220"
-      >
+      <el-table-column align="center" label="编号" width="220">
         <template slot-scope="scope">{{ scope.row.id }}</template>
       </el-table-column>
-      <el-table-column
-        align="center"
-        label="角色名"
-        width="220"
-      >
+      <el-table-column align="center" label="角色名" width="220">
         <template slot-scope="scope">{{ scope.row.name }}</template>
       </el-table-column>
-      <el-table-column
-        align="center"
-        label="说明"
-      >
+      <el-table-column align="center" label="说明">
         <template slot-scope="scope">{{ scope.row.description }}</template>
       </el-table-column>
-      <el-table-column
-        align="center"
-        label="操作"
-      >
+      <el-table-column align="center" label="操作">
         <template slot-scope="scope">
-          <el-button
-            type="primary"
-            size="small"
-            @click="handleEdit(scope)"
-          >编辑</el-button>
-          <el-button
-            type="danger"
-            size="small"
-            @click="handleDelete(scope)"
-          >删除</el-button>
+          <el-button type="primary" size="small" @click="handleEdit(scope)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDelete(scope)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-dialog
-      :visible.sync="dialogVisible"
-      :title="dialogType==='edit'?'编辑角色':'新建角色'"
-    >
-      <el-form
-        :model="role"
-        label-width="80px"
-        label-position="left"
-      >
+    <el-dialog :visible.sync="dialogVisible" :title="dialogType==='edit'?'编辑角色':'新建角色'">
+      <el-form :model="role" label-width="80px" label-position="left">
         <el-form-item label="角色名">
-          <el-input
-            v-model="role.name"
-            placeholder="输入角色名"
-          />
+          <el-input v-model="role.name" placeholder="输入角色名" />
         </el-form-item>
         <el-form-item label="角色说明">
           <el-input
@@ -91,37 +56,31 @@
         </el-form-item>
       </el-form>
       <div style="text-align:right;">
-        <el-button
-          type="danger"
-          @click="dialogVisible=false"
-        >取消</el-button>
-        <el-button
-          type="primary"
-          @click="confirmRole"
-        >保存</el-button>
+        <el-button type="danger" @click="dialogVisible=false">取消</el-button>
+        <el-button type="primary" @click="confirmRole">保存</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import path from 'path'
-import { deepClone } from '@/utils'
+import path from "path";
+import { deepClone } from "@/utils";
 import {
   getRoutes,
   getRoles,
   addRole,
   deleteRole,
   updateRole
-} from '@/api/role'
+} from "@/api/role";
 
 const defaultRole = {
-  id: '',
-  name: '',
-  description: '',
+  id: "",
+  name: "",
+  description: "",
   // routes: [],
   routerIds: []
-}
+};
 
 export default {
   data() {
@@ -131,42 +90,42 @@ export default {
       routes: [],
       rolesList: [],
       dialogVisible: false,
-      dialogType: 'new',
+      dialogType: "new",
       checkStrictly: false,
       form: {
         role: {
-          id: '',
-          name: '',
-          description: ''
+          id: "",
+          name: "",
+          description: ""
         },
         routerIds: []
       },
       defaultProps: {
-        children: 'children',
-        label: 'label'
+        children: "children",
+        label: "label"
       }
-    }
+    };
   },
   computed: {
     routesData() {
-      return this.routes
+      return this.routes;
     }
   },
   created() {
     // Mock: get all routes and roles list from server
-    this.getRouters()
-    this.getRoles()
+    this.getRouters();
+    this.getRoles();
   },
   methods: {
     async getRouters() {
-      const res = await getRoutes(this.$store.getters.rootRoleId)
-      this.serviceRoutes = res.data
-      this.routes = this.generateRoutes(res.data)
+      const res = await getRoutes(this.$store.getters.rootRoleId);
+      this.serviceRoutes = res.data;
+      this.routes = this.generateRoutes(res.data);
     },
     async getRoles() {
-      const res = await getRoles()
-      this.listLoading = false
-      this.rolesList = res.data
+      const res = await getRoles();
+      this.listLoading = false;
+      this.rolesList = res.data;
     },
 
     getCheckedRoute() {
@@ -177,23 +136,23 @@ export default {
       // );
     },
     // Reshape the routes structure so that it looks the same as the sidebar
-    generateRoutes(routes, basePath = '/') {
+    generateRoutes(routes, basePath = "/") {
       // console.log(routes)
-      const res = []
+      const res = [];
 
       for (let route of routes) {
         // skip some route
         if (route.hidden) {
-          continue
+          continue;
         }
 
         const onlyOneShowingChild = this.onlyOneShowingChild(
           route.children,
           route
-        )
+        );
 
         if (route.children && onlyOneShowingChild && !route.alwaysShow) {
-          route = onlyOneShowingChild
+          route = onlyOneShowingChild;
         }
 
         const data = {
@@ -201,71 +160,71 @@ export default {
           title: route.meta && route.meta.title,
           label: route.label,
           id: route.id
-        }
+        };
 
         // recursive child routes
         if (route.children) {
-          data.children = this.generateRoutes(route.children, data.path)
+          data.children = this.generateRoutes(route.children, data.path);
         }
-        res.push(data)
+        res.push(data);
       }
-      return res
+      return res;
     },
     generateArr(routes) {
-      let data = []
+      let data = [];
       routes.forEach(route => {
-        data.push(route)
+        data.push(route);
         if (route.children) {
-          const temp = this.generateArr(route.children)
+          const temp = this.generateArr(route.children);
           if (temp.length > 0) {
-            data = [...data, ...temp]
+            data = [...data, ...temp];
           }
         }
-      })
-      return data
+      });
+      return data;
     },
     handleAddRole() {
-      this.role = Object.assign({}, defaultRole)
+      this.role = Object.assign({}, defaultRole);
       if (this.$refs.tree) {
-        this.$refs.tree.setCheckedNodes([])
+        this.$refs.tree.setCheckedNodes([]);
       }
-      this.dialogType = 'new'
-      this.dialogVisible = true
+      this.dialogType = "new";
+      this.dialogVisible = true;
     },
     handleEdit(scope) {
-      this.dialogType = 'edit'
-      this.dialogVisible = true
-      this.checkStrictly = true
-      this.role = deepClone(scope.row)
+      this.dialogType = "edit";
+      this.dialogVisible = true;
+      this.checkStrictly = true;
+      this.role = deepClone(scope.row);
       this.$nextTick(() => {
         // const routes = this.generateRoutes(this.role.routes);
         // this.$refs.tree.setCheckedNodes(this.generateArr(routes));
         if (this.role.routerIds) {
-          this.$refs.tree.setCheckedKeys(this.role.routerIds)
+          this.$refs.tree.setCheckedKeys(this.role.routerIds);
         } else {
-          this.$refs.tree.setCheckedKeys([])
+          this.$refs.tree.setCheckedKeys([]);
         }
         // set checked state of a node not affects its father and child nodes
-        this.checkStrictly = false
-      })
+        this.checkStrictly = false;
+      });
     },
     handleDelete({ $index, row }) {
-      this.$confirm('确定删除角色吗?', 'Warning', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("确定删除角色吗?", "Warning", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       })
-        .then(async() => {
-          await deleteRole(row.id)
-          this.rolesList.splice($index, 1)
+        .then(async () => {
+          await deleteRole(row.id);
+          this.rolesList.splice($index, 1);
           this.$message({
-            type: 'success',
-            message: '删除成功!'
-          })
+            type: "success",
+            message: "删除成功!"
+          });
         })
         .catch(err => {
-          console.error(err)
-        })
+          console.error(err);
+        });
     },
     // generateTree(routes, basePath = '/', checkedKeys) {
     //   const res = []
@@ -292,11 +251,11 @@ export default {
     //   return res
     // },
     async confirmRole() {
-      const isEdit = this.dialogType === 'edit'
+      const isEdit = this.dialogType === "edit";
 
       const checkedKeys = this.$refs.tree
         .getCheckedKeys()
-        .concat(this.$refs.tree.getHalfCheckedKeys())
+        .concat(this.$refs.tree.getHalfCheckedKeys());
       // this.role.routes = this.generateTree(
       // deepClone(this.serviceRoutes),
       // "/",
@@ -304,65 +263,65 @@ export default {
       // );
 
       if (isEdit) {
-        this.form.role.name = this.role.name
-        this.form.role.description = this.role.description
-        this.form.role.id = this.role.id
-        this.form.routerIds = checkedKeys
-        await updateRole(this.role.id, this.form)
+        this.form.role.name = this.role.name;
+        this.form.role.description = this.role.description;
+        this.form.role.id = this.role.id;
+        this.form.routerIds = checkedKeys;
+        await updateRole(this.role.id, this.form);
         // for (let index = 0; index < this.rolesList.length; index++) {
         //   if (this.rolesList[index].id === this.role.id) {
         //     this.rolesList.splice(index, 1, Object.assign({}, this.role));
         //     break;
         //   }
         // }
-        this.getRoles()
+        this.getRoles();
       } else {
         this.form.routeIds = this.$refs.tree
           .getCheckedKeys()
-          .concat(this.$refs.tree.getHalfCheckedKeys())
-        const { data } = await addRole(this.form)
-        this.role.id = this.form.role.id
-        this.role.name = this.form.role.name
-        this.role.description = this.form.role.description
+          .concat(this.$refs.tree.getHalfCheckedKeys());
+        const { data } = await addRole(this.form);
+        this.role.id = this.form.role.id;
+        this.role.name = this.form.role.name;
+        this.role.description = this.form.role.description;
         // this.role.id = data.role.id;
         // this.rolesList.push(this.form.role);
-        this.getRoles()
+        this.getRoles();
       }
 
-      const { description, key, name, id } = this.role
-      this.dialogVisible = false
+      const { description, key, name, id } = this.role;
+      this.dialogVisible = false;
       this.$notify({
-        title: '成功',
+        title: "成功",
         dangerouslyUseHTMLString: true,
         message: `
             <div>角色名: ${name}</div>
             <div>说明: ${description}</div>
           `,
-        type: 'success'
-      })
+        type: "success"
+      });
     },
     // reference: src/view/layout/components/Sidebar/SidebarItem.vue
     onlyOneShowingChild(children = [], parent) {
-      let onlyOneChild = null
-      const showingChildren = children.filter(item => !item.hidden)
+      let onlyOneChild = null;
+      const showingChildren = children.filter(item => !item.hidden);
 
       // When there is only one child route, the child route is displayed by default
       if (showingChildren.length === 1) {
-        onlyOneChild = showingChildren[0]
-        onlyOneChild.path = path.resolve(parent.path, onlyOneChild.path)
-        return onlyOneChild
+        onlyOneChild = showingChildren[0];
+        onlyOneChild.path = path.resolve(parent.path, onlyOneChild.path);
+        return onlyOneChild;
       }
 
       // Show parent if there are no child route to display
       if (showingChildren.length === 0) {
-        onlyOneChild = { ...parent, path: '', noShowingChildren: true }
-        return onlyOneChild
+        onlyOneChild = { ...parent, path: "", noShowingChildren: true };
+        return onlyOneChild;
       }
 
-      return false
+      return false;
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
--- src/views/account/user.vue
+++ src/views/account/user.vue
@@ -1,30 +1,21 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input
-        v-model="listQuery.name"
-        class="filter-item"
-        style="width:200px"
-        placeholder="用户名"
-      />
+      <el-input v-model="listQuery.name" class="filter-item" style="width:200px" placeholder="用户名" />
       <el-button
         v-waves
         class="filter-item"
         type="primary"
         icon="el-icon-search"
         @click="handleFilter"
-      >
-        搜索
-      </el-button>
+      >搜索</el-button>
       <el-button
         class="filter-item"
         style="margin-left: 10px;"
         type="primary"
         icon="el-icon-edit"
         @click="handleCreate"
-      >
-        添加
-      </el-button>
+      >添加</el-button>
       <el-button
         v-waves
         :loading="downloadLoading"
@@ -32,9 +23,7 @@
         type="primary"
         icon="el-icon-download"
         @click="handleDownload"
-      >
-        导出
-      </el-button>
+      >导出</el-button>
 
       <el-table
         v-loading="listLoading"
@@ -46,50 +35,23 @@
         style="width: 100%;margin-top:30px;"
         @sort-change="sortChange"
       >
-        <el-table-column
-          align="center"
-          label="编号"
-          width="220"
-        >
+        <el-table-column align="center" label="编号" width="220">
           <template slot-scope="scope">{{ scope.row.id }}</template>
         </el-table-column>
-        <el-table-column
-          align="center"
-          label="用户名"
-          width="320"
-        >
+        <el-table-column align="center" label="用户名" width="320">
           <template slot-scope="scope">{{ scope.row.username }}</template>
         </el-table-column>
-        <el-table-column
-          align="center"
-          label="密码"
-          width="400"
-        >
+        <el-table-column align="center" label="密码" width="400">
           <template slot-scope="scope">{{ scope.row.password }}</template>
         </el-table-column>
 
-        <el-table-column
-          align="center"
-          label="角色"
-          width="400"
-        >
-          <template slot-scope="scope">{{ scope.row.roles }}</template>
+        <el-table-column align="center" label="角色" width="400">
+          <template slot-scope="scope">{{ scope.row.roleNames}}</template>
         </el-table-column>
-        <el-table-column
-          align="center"
-          label="操作"
-        >
+        <el-table-column align="center" label="操作">
           <template slot-scope="scope">
-            <el-button
-              type="primary"
-              size="small"
-              @click="handleEdit(scope)"
-            >编辑</el-button>
-            <el-button
-              type="danger"
-              size="small"
-              @click="handleDelete(scope)"
-            >删除</el-button>
+            <el-button type="primary" size="small" @click="handleEdit(scope)">编辑</el-button>
+            <el-button type="danger" size="small" @click="handleDelete(scope)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -103,28 +65,13 @@
         @pagination="fetchData"
       />
 
-      <el-dialog
-        :visible.sync="dialogVisible"
-        :title="dialogType==='edit'?'编辑用户':'新建用户'"
-      >
-        <el-form
-          :model="user"
-          label-width="80px"
-          label-position="left"
-        >
+      <el-dialog :visible.sync="dialogVisible" :title="dialogType==='edit'?'编辑用户':'新建用户'">
+        <el-form :model="user" label-width="80px" label-position="left">
           <el-form-item label="用户名">
-            <el-input
-              v-model="user.username"
-              placeholder="输入用户名"
-            />
+            <el-input v-model="user.username" placeholder="输入用户名" />
           </el-form-item>
           <el-form-item label="密码">
-            <el-input
-              v-model="user.password"
-              maxlength="10"
-              show-password
-              placeholder="密码"
-            />
+            <el-input v-model="user.password" maxlength="10" show-password placeholder="密码" />
           </el-form-item>
           <el-form-item label="角色">
             <!-- <el-tree
@@ -137,29 +84,15 @@
               node-key="id"
               class="permission-tree"
               @check-change="getCheckedRoute"
-            /> -->
-            <el-select
-              v-model="roleName"
-              placeholder="请选择"
-            >
-              <el-option
-                v-for="item in roles"
-                :key="item.id"
-                :value="item.id"
-                :label="item.name"
-              />
+            />-->
+            <el-select ref="select" v-model="roleName" placeholder="请选择">
+              <el-option v-for="item in roles" :key="item.id" :value="item.id" :label="item.name" />
             </el-select>
           </el-form-item>
         </el-form>
         <div style="text-align:right;">
-          <el-button
-            type="danger"
-            @click="dialogVisible=false"
-          >取消</el-button>
-          <el-button
-            type="primary"
-            @click="confirmUser"
-          >保存</el-button>
+          <el-button type="danger" @click="dialogVisible=false">取消</el-button>
+          <el-button type="primary" @click="confirmUser">保存</el-button>
         </div>
       </el-dialog>
     </div>
@@ -167,18 +100,24 @@
 </template>
 
 <script>
-import Pagination from '@/components/Pagination'
-import waves from '@/directive/waves'
-import { parseTime } from '@/utils'
-import { addUser, getUserList, deleteUser, updateUser } from '@/api/user'
+import Pagination from "@/components/Pagination";
+import waves from "@/directive/waves";
+import { parseTime } from "@/utils";
+import { addUser, getUserList, deleteUser, updateUser } from "@/api/user";
+import { getRoles } from "@/api/role";
 
 const defaultUser = {
-  id: '',
-  username: '',
-  password: '',
+  id: "",
+  username: "",
+  password: "",
   // routes: [],
   roleIds: []
-}
+};
+
+const defaultRole = {
+  id: "",
+  name: ""
+};
 
 export default {
   components: { Pagination },
@@ -191,97 +130,119 @@ export default {
       listQuery: {
         pageNum: 1,
         pageSize: 5,
-        orderKey: 'id',
-        orderType: 'desc'
+        orderKey: "id",
+        orderType: "desc"
+      },
+      form: {
+        username: "",
+        password: "",
+        roleIds: []
       },
       user: Object.assign({}, defaultUser),
-      value: '',
+      role: Object.assign({}, defaultRole),
+      value: "",
       sortOptions: [
-        { label: '默认', key: '-id' },
-        { label: '倒序', key: '+id' }
+        { label: "默认", key: "-id" },
+        { label: "倒序", key: "+id" }
       ],
-      user: {
-        name: '',
-        password: ''
-      },
+      // user: {
+      //   name: '',
+      //   password: ''
+      // },
       roles: [],
-      roleName: '',
+      roleName: "",
       dialogVisible: false,
-      dialogType: 'new',
+      dialogType: "new",
       downloadLoading: false
-    }
+    };
   },
 
   created() {
-    this.getUsers()
-    this.getRoles()
+    this.getUsers();
+    this.getRoles();
   },
   methods: {
     async getUsers() {
-      this.listLoading = false
+      const res = await getUserList(this.listQuery);
+      this.list = res.data.list;
+      this.listLoading = false;
     },
-    async getRoles() {},
-    fetchData() {
-      this.listLoading = false
+    async getRoles() {
+      const res = await getRoles();
+      this.roles = res.data;
+      this.listLoading = false;
     },
+    fetchData() {},
     handleCreate() {
-      this.user = Object.assign({}, defaultUser)
-      this.dialogType = 'new'
-      this.dialogVisible = true
+      this.user = Object.assign({}, defaultUser);
+      this.dialogType = "new";
+      this.dialogVisible = true;
       // this.$router.push('/staff/addStaff')
     },
     handleFilter(val) {
-      this.listQuery.pageNum = 1
-      console.log('sortVal:' + val)
+      this.listQuery.pageNum = 1;
+      console.log("sortVal:" + val);
       //  if(val instanceof String){
       try {
-        if (val.startsWith('+')) {
-          this.listQuery.orderType = 'asc'
-        } else if (val.startsWith('-')) {
-          this.listQuery.orderType = 'desc'
+        if (val.startsWith("+")) {
+          this.listQuery.orderType = "asc";
+        } else if (val.startsWith("-")) {
+          this.listQuery.orderType = "desc";
         }
-        this.listQuery.orderKey = val.substring(1)
+        this.listQuery.orderKey = val.substring(1);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      this.fetchData()
+      this.fetchData();
     },
 
     sortChange(data) {
-      const { prop, order } = data
-      console.error('prop:' + prop + ' order:' + order)
-      this.handleFilter()
+      const { prop, order } = data;
+      console.error("prop:" + prop + " order:" + order);
+      this.handleFilter();
     },
 
-    confirmUser() {},
+    async confirmUser() {
+      const isEdit = this.dialogType === "edit";
+      const checkedRoleId = this.$refs.select.value;
+
+      if (isEdit) {
+      } else {
+        this.user.roleIds = [];
+        this.user.roleIds.push(this.$refs.select.value);
+        await addUser(this.user);
+        this.dialogVisible = false;
+        this.getUsers();
+      }
+    },
 
     handleDownload() {
-      this.downloadLoading = true
-      import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['姓名', '入职日期', '性别', '手机号码', '备注']
-        const filterVal = ['name', 'entryDate', 'sex', 'phone', 'mark']
-        const data = this.formatJson(filterVal, this.list)
+      this.downloadLoading = true;
+      import("@/vendor/Export2Excel").then(excel => {
+        const tHeader = ["姓名", "入职日期", "性别", "手机号码", "备注"];
+        const filterVal = ["name", "entryDate", "sex", "phone", "mark"];
+        const data = this.formatJson(filterVal, this.list);
         excel.export_json_to_excel({
           header: tHeader,
           data,
-          filename: '学员列表'
-        })
-        this.downloadLoading = false
-      })
+          filename: "学员列表"
+        });
+        this.downloadLoading = false;
+      });
     },
     formatJson(filterVal, jsonData) {
       return jsonData.map(v =>
         filterVal.map(j => {
-          if (j === 'startDate' || j === 'endDate') {
-            return parseTime(v[j], '{y}-{m}-{d}')
+          if (j === "startDate" || j === "endDate") {
+            return parseTime(v[j], "{y}-{m}-{d}");
           } else {
-            return v[j]
+            return v[j];
           }
         })
-      )
+      );
     }
   }
-}
+};
 </script>
 
 <style>
